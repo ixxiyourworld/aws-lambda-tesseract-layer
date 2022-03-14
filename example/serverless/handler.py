b5405992c515efbc9e8f4cb62d5a487ec02ad802
Luc Hendriks <luc.hendriks@gmail.com>
2022-03-14 11:33:36 +0100

    Update handler.py

--- example/serverless/handler.py
+++ example/serverless/handler.py
@@ -2,12 +2,16 @@ from PIL import Image
 import requests
 import pytesseract
 from io import BytesIO
-
+import json
 
 def main(evt, ctx):
     url = evt["body"]
     res = requests.get(url)
     img = Image.open(BytesIO(res.content))
-    txt = pytesseract.image_to_string(img, lang="deu")
-    return {"statusCode": 200, "body": txt}
+    txt = pytesseract.image_to_string(img)
+    return {"statusCode": 200, "body": json.dumps({
+        'txt': txt,
+        'key': url,
+        'method': 'ocr'
+    })}
 
